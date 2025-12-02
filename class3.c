9d8dd0e2d395ae4f9a557a89aff129992736d8f1
sabbir123-dev <mdsabbirahmedgopalgonj@gmail.com>
2025-12-02 12:02:35 +0600

    Add files via upload
    updated

--- class3.c
+++ class3.c
@@ -1,50 +1,47 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 
 int stack[100];
-int top=-1;
-void push(int x){
+int top = -1;
 
-stack[++top]=x;
-
-
+void push(int x) {
+    stack[++top] = x;
 }
-int pop(){
-return stack[top--];
+
+int pop() {
+    return stack[top--];
 }
-int postfixevaluation(char *exp){
-int i=0;
-char ch;
-while((ch==exp[i++])!='\0'){
 
-    if(isdigit(ch)){
-        push(ch -'0');
+int postfixevaluation(char *exp) {
+    int i = 0;
+    char ch;
 
-    }
-    else if(exp[i]==' '){
-        continue;
-    }
-    else{
-        int v1= pop();
-        int v2= pop();
+    while ((ch = exp[i++]) != '\0') {
 
-        switch (ch){
-        case '+': push(v2+v1);break;
-        case '-': push(v2-v1);break;
-        case '*': push(v2*v1);break;
-        case '/': push(v2/v1);break;
+        if (isdigit(ch)) {
+            push(ch - '0');
+        }
+        else if (ch == ' ') {
+            continue;
+        }
+        else {
+            int v1 = pop();
+            int v2 = pop();
 
+            switch (ch) {
+                case '+': push(v2 + v1); break;
+                case '-': push(v2 - v1); break;
+                case '*': push(v2 * v1); break;
+                case '/': push(v2 / v1); break;
+            }
         }
     }
-}
-return pop();
-
+    return pop();
 }
 
-int main(){
-
-char expression[] = "53-8*";
-printf("Result :%d", postfixevaluation(expression));
-return 0;
+int main() {
+    char expression[] = "53-8*";
+    printf("Result : %d", postfixevaluation(expression));
+    return 0;
 }
 
